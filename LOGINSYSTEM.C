31a607706c41d5be8161d88b402e0746f710e381
Nihar Koli <42617538+nihar-koli@users.noreply.github.com>
2019-04-15 22:56:24 +0530

    Added Login system program

---
+++ LOGINSYSTEM.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char str[50];
+  int i=0;
+      clrscr();
+   printf("\n\n Password : ");
+   do
+   {
+     str[i] = getch();
+     i++;
+     printf("\b* ");
+   }while((int)str[i-1] != 13);
+   str[i-1]='\0';
+     printf("\n\n Password is : %s",str);
+
+   if(strcmp(str, "password") == 0)
+   {
+      printf("\n\n succesfull...");
+   }
+   else
+   {
+      printf("\n\n Try again...");
+   }
+
+
+   getch();
+
+}
